e2e13e299f44240ec581f12e3918425d15902d39
bagusarya._ <aryawirianda@gmail.com>
2025-12-18 08:56:44 +0800

    fixed code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,23 +6,13 @@ import {
   approveUser,
 } from "../controllers/authController.js";
 
-
 const router = express.Router();
 
-// ===================
-// PUBLIC ROUTES
-// ===================
 router.post("/register", register);
 router.post("/login", login);
 
-// ===================
-// PROTECTED ROUTES
-// ===================
 router.post("/logout", logout);
 
-// ===================
-// SUPERADMIN ONLY
-// ===================
 router.post(
   "/approve",
   approveUser
